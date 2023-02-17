3d17bf4c9c33e291b2787da5a66461b4e2030511
martinkubicka <martinkubicka22@gmail.com>
2023-02-17 12:42:47 +0100

    rozdelenie

---
+++ IDS_2.sql
@@ -0,0 +1,6 @@
+/*
+* ROZDELENIE: 
+* Martin: Entity: Nakup (+ generalizace), Lekarna | Vztahy: Obsahuje, Byl proveden | Ukazkove data  
+*
+* Matej: Entity: Lek, Poistovna | Vztahy: Skladuje, Hradi | Ukazkove data
+*/
