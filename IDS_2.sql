bf8e9b641d7f7cab0167bf14fc71209411ec97e7
xmacek27 <matej.macek@email.cz>
2023-02-18 20:48:22 +0100

    Add more inserts

--- IDS_2.sql
+++ IDS_2.sql
@@ -50,4 +50,20 @@ INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Paralen', 29.90, 
 INSERT INTO Pojistovna (pojistovna_nazev, pojistovna_sidlo) VALUES ('VZP', 'Praha 2');
 INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES (1, 1, 50);
 INSERT INTO Hradi (lek_pk, pojistovna_pk,castka) VALUES (1, 1, 10);
+INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Aspirin', 39.90, 0);
+INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Ibuprofen', 49.90, 1);
+INSERT INTO Pojistovna (pojistovna_nazev, pojistovna_sidlo) VALUES ('Česká pojišťovna', 'Praha 1');
+INSERT INTO Pojistovna (pojistovna_nazev, pojistovna_sidlo) VALUES ('Uniqa', 'Brno');
+INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES (1, 2, 30);
+INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES (2, 3, 20);
+INSERT INTO Hradi (lek_pk, pojistovna_pk,castka) VALUES (2, 2, 20);
+INSERT INTO Hradi (lek_pk, pojistovna_pk,castka) VALUES (3, 1, 15);
+INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Strepsils', 69.90, 0);
+INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Xanax', 159.90, 1);
+INSERT INTO Pojistovna (pojistovna_nazev, pojistovna_sidlo) VALUES ('Zdravotní pojišťovna ministerstva vnitra', 'Praha 3');
+INSERT INTO Pojistovna (pojistovna_nazev, pojistovna_sidlo) VALUES ('Maxima pojišťovna', 'Bratislava');
+INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES (2, 1, 100);
+INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES (3, 2, 10);
+INSERT INTO Hradi (lek_pk, pojistovna_pk,castka) VALUES (1, 2, 15);
+INSERT INTO Hradi (lek_pk, pojistovna_pk,castka) VALUES (3, 2, 20);
 
