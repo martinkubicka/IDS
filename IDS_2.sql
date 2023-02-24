fbb02c49ba4d9291a683d3e40d6a24e6b6e4890d
martinkubicka <martinkubicka22@gmail.com>
2023-02-24 19:24:20 +0100

    syntax stuff and many other things fixed..

--- IDS_2.sql
+++ IDS_2.sql
@@ -7,20 +7,30 @@
 -- @date 26.3.2023
 
 --------- Reset tables ---------
+DROP TABLE Skladuje;
+DROP TABLE Obsahuje;
+DROP TABLE Hradi;
+DROP TABLE Nakup_na_predpis;
+DROP TABLE Nakup;
+DROP TABLE Lekarna;
 DROP TABLE Lek;
 DROP TABLE Pojistovna;
-DROP TABLE Skladuje;
-DROP TABLE Hradi;
-DROP TABLE Nakup;
-DROP TABLE Nakup_na_predpis;
-DROP TABLE Lekarna;
-DROP TABLE Obsahuje;
 
 --------- Entities ---------
+CREATE TABLE Lekarna (
+    lekarna_pk NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    lekarna_nazev VARCHAR2(50) NOT NULL,
+    lekarna_jmeno_majitele VARCHAR2(50) NOT NULL,
+    lekarna_ulice VARCHAR2(50) NOT NULL,
+    lekarna_psc NUMBER(10) NOT NULL,
+    lekarna_mesto VARCHAR2(50) NOT NULL,
+    lekarna_stat VARCHAR2(50) NOT NULL
+);
+
 CREATE TABLE Nakup (
     nakup_pk NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nakup_datum DATE NOT NULL,
-    nakup_suma NUMBER(10) NOT NULL,
+    nakup_suma NUMBER(10, 2) NOT NULL,
     lekarna_pk NUMBER NOT NULL, -- relationship 0..* to 1
     CONSTRAINT byl_proveden_ck_lekarna
         FOREIGN KEY (lekarna_pk)
@@ -32,26 +42,17 @@ CREATE TABLE Nakup (
 -- Also it has special atribute rodne_cislo which needs to be in special format
 CREATE TABLE Nakup_na_predpis (
     nakup_pk NUMBER NOT NULL PRIMARY KEY,
-    rodne_cislo NUMBER NOT NULL CHECK(REGEXP_LIKE(rodne_cislo, '^[0-9]{6}\/?[0-9]{3,4}$')),
+    rodne_cislo VARCHAR2(11) NOT NULL CHECK(REGEXP_LIKE(rodne_cislo, '^[0-9]{6}\/?[0-9]{3,4}$')),
     CONSTRAINT nakup_na_predpis_ck
         FOREIGN KEY (nakup_pk)
         REFERENCES Nakup (nakup_pk)
 );
 
-CREATE TABLE Lekarna (
-    lekarna_pk NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    lekarna_nazev VARCHAR2(50) NOT NULL,
-    lekarna_jmeno_majitele VARCHAR2(50) NOT NULL,
-    lekarna_ulice VARCHAR2(50) NOT NULL,
-    lekarna_psc NUMBER(10) NOT NULL,
-    lekarna_mesto VARCHAR2(50) NOT NULL,
-    lekarna_stat VARCHAR2(50) NOT NULL
-);
 
 CREATE TABLE Lek (
     lek_pk NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     lek_nazev VARCHAR2(50) NOT NULL,
-    lek_cena NUMBER(10) NOT NULL,
+    lek_cena NUMBER(10, 2) NOT NULL,
     lek_na_predpis  NUMBER(1) NOT NULL CHECK(REGEXP_LIKE(lek_na_predpis, '^(0|1)$'))
 );
 
@@ -83,22 +84,23 @@ CREATE TABLE Skladuje (
         REFERENCES Lekarna (lekarna_pk),
     CONSTRAINT skladuje_ck_lek
         FOREIGN KEY (lek_pk)
-        REFERENCES Lek (lek_pk)    
+        REFERENCES Lek (lek_pk)
 );
 
 CREATE TABLE Hradi (
     pojistovna_pk NUMBER NOT NULL,
     lek_pk NUMBER NOT NULL,
-    castka NUMBER(10) NOT NULL,
+    castka NUMBER(10, 2) NOT NULL,
     CONSTRAINT hradi_ck_pojistovna
         FOREIGN KEY (pojistovna_pk)
-        REFERENCES Pojistovna (pojistovna_pk)
+        REFERENCES Pojistovna (pojistovna_pk),
     CONSTRAINT hradi_ck_lek
         FOREIGN KEY (lek_pk)
-        REFERENCES Lek (lek_pk),
+        REFERENCES Lek (lek_pk)
 );
 
 --------- Example data ---------
+
 INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Paralen', 29.90, 0);
 INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Aspirin', 39.90, 0);
 INSERT INTO Lek (lek_nazev, lek_cena, lek_na_predpis) VALUES ('Strepsils', 69.90, 0);
@@ -118,8 +120,8 @@ INSERT INTO Nakup (nakup_datum, nakup_suma, lekarna_pk) VALUES (TO_DATE('17.02.2
 INSERT INTO Nakup (nakup_datum, nakup_suma, lekarna_pk) VALUES (TO_DATE('17.02.2023', 'DD.MM.YYYY'), 159.60, (SELECT lekarna_pk from Lekarna WHERE lekarna_nazev='Nejlepší lékárna')); 
 INSERT INTO Nakup (nakup_datum, nakup_suma, lekarna_pk) VALUES (TO_DATE('19.02.2023', 'DD.MM.YYYY'), 369.6, (SELECT lekarna_pk from Lekarna WHERE lekarna_nazev='Nejlepší lékárna'));
 
-INSERT INTO Nakup_na_predpis (nakup_pk, rodne_cislo) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='17.02.2023' and nakup_suma=159.60), '020220/1234');
-INSERT INTO Nakup_na_predpis (nakup_pk, rodne_cislo) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='19.02.2023' and nakup_suma=369.60), '9815121111');
+INSERT INTO Nakup_na_predpis (nakup_pk, rodne_cislo) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('17.02.2023', 'DD.MM.YYYY') and nakup_suma=159.60), '020220/1234');
+INSERT INTO Nakup_na_predpis (nakup_pk, rodne_cislo) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('19.02.2023', 'DD.MM.YYYY') and nakup_suma=369.60), '981512111');
 
 INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES ((SELECT lekarna_pk from Lekarna WHERE lekarna_nazev='Vaše lékárna'), (SELECT lek_pk from Lek WHERE lek_nazev='Paralen'), 50);
 INSERT INTO Skladuje (lekarna_pk, lek_pk, mnozstvi) VALUES ((SELECT lekarna_pk from Lekarna WHERE lekarna_nazev='Vaše lékárna'), (SELECT lek_pk from Lek WHERE lek_nazev='Aspirin'), 30);
@@ -133,11 +135,10 @@ INSERT INTO Hradi (pojistovna_pk, lek_pk, castka) VALUES ((SELECT pojistovna_pk 
 INSERT INTO Hradi (pojistovna_pk, lek_pk, castka) VALUES ((SELECT pojistovna_pk from Pojistovna WHERE pojistovna_nazev='VZP'), (SELECT lek_pk from Lek WHERE lek_nazev='Xanax'), 15);
 INSERT INTO Hradi (pojistovna_pk, lek_pk, castka) VALUES ((SELECT pojistovna_pk from Pojistovna WHERE pojistovna_nazev='VZP'), (SELECT lek_pk from Lek WHERE lek_nazev='Paralen'), 10);
 
-INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='17.02.2023' and nakup_suma=58.80), (SELECT lek_pk from Lek WHERE lek_nazev='Paralen'), 2);
-INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='17.02.2023' and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Paralen'), 1);
-INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='17.02.2023' and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Aspirin'), 2);
-INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='17.02.2023' and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Ibuprofen'), 1);
-INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='19.02.2023' and nakup_suma=369.60), (SELECT lek_pk from Lek WHERE lek_nazev='Strepsils'), 3);
-INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum='19.02.2023' and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Ibuprofen'), 1);
+INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('17.02.2023', 'DD.MM.YYYY') and nakup_suma=59.80), (SELECT lek_pk from Lek WHERE lek_nazev='Paralen'), 2);
+INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('17.02.2023', 'DD.MM.YYYY') and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Paralen'), 1);
+INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('17.02.2023', 'DD.MM.YYYY') and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Aspirin'), 2);
+INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('17.02.2023', 'DD.MM.YYYY') and nakup_suma=159.60), (SELECT lek_pk from Lek WHERE lek_nazev='Ibuprofen'), 1);
+INSERT INTO Obsahuje (nakup_pk, lek_pk, mnozstvi) VALUES ((SELECT nakup_pk from Nakup WHERE nakup_datum=TO_DATE('19.02.2023', 'DD.MM.YYYY') and nakup_suma=369.60), (SELECT lek_pk from Lek WHERE lek_nazev='Strepsils'), 3);
 
 --------- End of IDS_2.sql ---------
